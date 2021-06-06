a84010b88fed5cd6a91e28c572fcc295d726adc5
Lorenzo Aidala <Lorenzo Aidala@DESKTOP-21723CJ>
2021-06-06 12:16:49 +0200

    Model funzionante, manca la visita del grafo

--- src/main/java/it/polito/tdp/borders/model/Country.java
+++ src/main/java/it/polito/tdp/borders/model/Country.java
@@ -1,6 +1,6 @@
 package it.polito.tdp.borders.model;
 
-public class Country {
+public class Country implements Comparable<Country>{
 
 	private int codice;
 	private String nomeAbb;
@@ -29,9 +29,33 @@ public class Country {
 	public void setNomeComp(String nomeComp) {
 		this.nomeComp = nomeComp;
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + codice;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Country other = (Country) obj;
+		if (codice != other.codice)
+			return false;
+		return true;
+	}
 	@Override
 	public String toString() {
 		return "Country [codice=" + codice + ", nomeAbb=" + nomeAbb + ", nomeComp=" + nomeComp + "]";
+	}
+	public int compareTo(Country o) {
+		return this.nomeAbb.compareTo(o.nomeAbb);
 	}
 	
 	
--- src/main/java/it/polito/tdp/borders/model/Model.java
+++ src/main/java/it/polito/tdp/borders/model/Model.java
@@ -1,7 +1,6 @@
 package it.polito.tdp.borders.model;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import org.jgrapht.Graph;
 import org.jgrapht.Graphs;
@@ -13,6 +12,7 @@ import it.polito.tdp.borders.db.BordersDAO;
 public class Model {
 	private Graph <Country, DefaultEdge> grafo;
 	private Map<Integer,Country> idMap;
+	private List<Country> countries;
 	private BordersDAO dao;
 
 	public Model() {
@@ -25,13 +25,20 @@ public class Model {
 		grafo = new SimpleGraph<>(DefaultEdge.class);
 		
 		//Graphs.addAllVertices(this.grafo, idMap.values());
-		
+		if(dao.getCountryPairs(idMap, anno).isEmpty()) {
+			System.out.println("Nessun confine disponibile per i paesi nell'anno selezionato");
+			return;
+		}
+			
+			
 		for(Border b : dao.getCountryPairs(idMap, anno)) {
 			this.grafo.addVertex(b.getC1());
 			this.grafo.addVertex(b.getC2());
 			this.grafo.addEdge(b.getC1(), b.getC2());
 		}
 		
+		countries = new ArrayList<Country>(this.grafo.vertexSet());
+		Collections.sort(countries);
 	}
 	
 	public int getNVertex() {
